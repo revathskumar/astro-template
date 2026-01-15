0220868d3ec1fe0fe3bc1a7f367b1f4fa761e31f
Revath S Kumar <rsk@revathskumar.com>
2026-01-15 20:35:47 +0100

    chore: add custom Optional generic type

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,5 @@
 /// <reference types="astro/client" />
 /// <reference types="vite-plugin-pwa/client" />
 /// <reference types="vite-plugin-pwa/info" />
+
+type Optional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>
